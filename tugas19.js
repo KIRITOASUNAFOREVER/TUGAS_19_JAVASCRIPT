643faa6548a933f7e32e1ff21245894b0df52023
EDWARD <60771749+KiritoEdward@users.noreply.github.com>
2021-03-26 14:16:57 +0700

    Tugas 19 Javascript
    Tugas ini Saya kumpulkan untuk NIOMIC

---
+++ tugas19.js
@@ -0,0 +1,16 @@
+console.log("Perulangan Repeat 1");
+let kata = "Semangat...";
+console.log(kata.repeat(5));
+console.log("\n");
+
+console.log("Perulangan Repeat 2");
+let bintang="*"
+console.log(bintang.repeat(5));
+console.log(bintang.repeat(25));
+console.log(bintang.repeat(45));
+console.log(bintang.repeat(55));
+console.log(bintang.repeat(85));
+console.log(bintang.repeat(55));
+console.log(bintang.repeat(45));
+console.log(bintang.repeat(25));
+console.log(bintang.repeat(5));
